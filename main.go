e98f1298ad5515847e6cc41f96f2cc2f0f397def
Antony Cleave <antony.cleave@nscale.com>
2025-02-20 10:50:45 +0000

    not sure how this works

--- main.go
+++ main.go
@@ -1,31 +1,67 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"net/http"
+	"os"
+	"sync"
+	"time"
 )
 
-var (
-	NWorkers = flag.Int("n", 4, "The number of workers to start")
-	HTTPAddr = flag.String("http", ":8000", "Address to listen for HTTP requests on")
-)
+type Task struct {
+	ID int
+}
+
+var taskQueue = make(chan Task) // Unbuffered channel for task processing
+var maxNics = 4 // Number of available nics
+var wg sync.WaitGroup
+
+func taskWorker() {
+	defer wg.Done()
+	for task := range taskQueue {
+		fmt.Printf("Processing Task ID: %d\n", task.ID)
+		// Simulate task processing time
+		time.Sleep(2 * time.Second)
+		fmt.Printf("Task ID %d completed\n", task.ID)
+	}
+}
+
+func submitTask(w http.ResponseWriter, r *http.Request, id int) {
+	fmt.Fprintf(w, "There are %d jobs in the queue\n", len(taskQueue))
+	if len(taskQueue) >= maxNics {
+		http.Error(w, "Too many tasks in the queue, try again later", http.StatusTooManyRequests)
+		return
+	}
+
+	// Create a new task and add it to the queue
+	task := Task{ID: id}
+
+	select {
+	case taskQueue <- task:
+		fmt.Fprintf(w, "Task ID %d added to queue\n", taskID)
+	default:
+		// If channel is full or there are too many tasks running, reject new task
+		http.Error(w, "Server is busy, try again later", http.StatusServiceUnavailable)
+	}
+}
 
 func main() {
-	// Parse the command-line flags.
-	flag.Parse()
-
-	// Start the dispatcher.
-	fmt.Println("Starting the dispatcher")
-	StartDispatcher(*NWorkers)
-
-	// Register our collector as an HTTP handler function.
-	fmt.Println("Registering the collector")
-	http.HandleFunc("/work", Collector)
-
-	// Start the HTTP server!
-	fmt.Println("HTTP server listening on", *HTTPAddr)
-	if err := http.ListenAndServe(*HTTPAddr, nil); err != nil {
-		fmt.Println(err.Error())
+	// Start workers (equal to the number of available nics)
+	for i := 0; i < maxNics; i++ {
+		//wg.Add(1)
+		go taskWorker()
 	}
+
+	// HTTP handler for submitting tasks
+	http.HandleFunc("/work", submitTask)
+
+	// Start HTTP server
+	fmt.Println("Starting server on port 8000...")
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+		os.Exit(1)
+	}
+
+	// Wait for all workers to finish before exiting
+	//wg.Wait()
 }
