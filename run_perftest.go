21aa085cf6297700ae9f4484e4dc469973ab4fb1
Antony Cleave <antony.cleave@nscale.com>
2025-02-21 13:37:28 +0000

    testing with sleep

--- run_perftest.go
+++ run_perftest.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-/*	"log"
-	"os"
-	"os/exec" */
+	//"log"
+	//"os"
+	"syscall"
+	"os/exec"
 	"fmt"
 	"strconv"
+	"strings"
 	"net"
 	"time"
 )
+const ibWriteClientWait uint64 = 5
+const ibWriteErrorWait = 100 * time.Millisecond
 
 func buildIBWriteBWArgs(myTask Task) ([]string) {
 	//this will convert the supplied args inti
@@ -33,28 +37,36 @@ func Listener() (port int, err error) {
 }
 
 
-func startIBWriteBW(myTask Task, nicIndex int) {
-    tcpPort, _ := Listener()
+func startIBWriteBW(myTask Task, nicIndex int) (error) {
+    cmd := "/opt/perftest-with-rocm/bin/ib_write_bw"
+	tcpPort, _ := Listener()
 	arglist := buildIBWriteBWArgs(myTask)
-    arglist = append([]string{"/opt/perftest-with-rocm/bin/ib_write_bw", "-p", fmt.Sprintf("%d", tcpPort), "-d", fmt.Sprintf("%s", NicList[nicIndex])}, arglist...)
-	fmt.Println(arglist)
-	// this will start an ib_write_bw_process
-	/*
-    cmd := exec.Command( arglist )
-    err := cmd.Start()
-    time.Sleep(time.Duration(myTask.Duration) * time.Second)
+
+    //arglist = append([]string{"-p", fmt.Sprintf("%d", tcpPort), "-d", fmt.Sprintf("%s", NicList[nicIndex])}, arglist...)
+	arglist = append([]string{"-p", fmt.Sprintf("%d", tcpPort), "-d", "bnxt_re9"}, arglist...)
+	cmd = "/opt/perftest-with-rocm/bin/ib_write_bw"
+	cmd="sleep"
+	arglist = []string{"3600"}
+	fmt.Printf("running %s \"%s\"\n", cmd, strings.Join(arglist, "\" "))
+
+	// this will start an ib_write_bw server process
+    ib_write_bw_cmd := exec.Command(cmd,  arglist... )
+	ib_write_bw_cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+    err := ib_write_bw_cmd.Start()
 	if err != nil {
         return err
+		fmt.Printf("Command finished with error: %v. the pid is/was %d\n", err, ib_write_bw_cmd.Process.Pid)
     }
-    pid := cmd.Process.Pid
-    // use goroutine waiting, manage process
-    // this is important, otherwise the process becomes in S mode
-    go func() {
-        err = cmd.Wait()
-        fmt.Printf("Command finished with error: %v", err)
-    }()
-    */
-	time.Sleep(time.Duration(myTask.Duration) * time.Second)
-    return
-
+	time.Sleep(time.Duration(myTask.Duration + ibWriteClientWait) * time.Second)
+	pid := ib_write_bw_cmd.Process.Pid
+    pgid, err := syscall.Getpgid(pid)
+	if err == nil {
+		fmt.Printf("Killing process. The pid is %d, and the pgid we are killing is %d\n", pid, pgid)
+		err := syscall.Kill(-pgid, syscall.SIGKILL)
+		if err != nil {
+			fmt.Printf("kill Command finished with error: %v. the pid is/was %d\n", err, pid)
+			return  err
+		}
+	}
+	return nil
 }
