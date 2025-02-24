33d43aace24e353b50991e26c784d99a72741fef
Antony Cleave <antony.cleave@nscale.com>
2025-02-24 15:33:53 +0000

    fix formatting

--- main.go
+++ main.go
@@ -1,55 +1,56 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 	"sync"
-	"encoding/json"
 )
 
 type Task struct {
-	ID uint64 `json:"id,omitempty"` // my task ID
-	Duration uint64 `json:"duration,omitempty"` // the time to measure the ib_write_bw run for in Seconds
-	QP uint64 `json:"qp,omitempty"`// the number of queue pairs to use
-	MsgSize uint64 `json:"msgsize,omitempty"`// the size of the message to send in bytes
-	IgnoreCPUSpeedWarnings bool `json:"ignorecpuspeedarnings,omitempty"`
-	OutputChannel OutputQueue `json:"-"`
+	ID                     uint64      `json:"id,omitempty"`       // my task ID
+	Duration               uint64      `json:"duration,omitempty"` // the time to measure the ib_write_bw run for in Seconds
+	QP                     uint64      `json:"qp,omitempty"`       // the number of queue pairs to use
+	MsgSize                uint64      `json:"msgsize,omitempty"`  // the size of the message to send in bytes
+	IgnoreCPUSpeedWarnings bool        `json:"ignorecpuspeedarnings,omitempty"`
+	OutputChannel          OutputQueue `json:"-"`
 }
 type TaskResult struct {
 	ServerPort int
-	Error error
+	Error      error
 }
 type JsonErrorResponse struct {
-	ErrorText string
+	ErrorText  string
 	StatusCode int
 }
 type JsonResponse struct {
-    TaskData Task
-	ServerPort int
+	TaskData    Task
+	ServerPort  int
 	MessageText string
-	StatusCode int
+	StatusCode  int
 }
+
 var taskQueue = make(chan Task) // Unbuffered channel for task processing
-var maxNics int            // Number of available nics
+var maxNics int                 // Number of available nics
 var wg sync.WaitGroup
 var taskID uint64 = 0 // counter for all jobs
 var NicList []string
 
 type OutputQueue chan TaskResult // creat a type to
 
-func  parseBodyToTask(r *http.Request) (Task, error) {
-    // converts json body to Task struct applying sane defaults if any are missing
+func parseBodyToTask(r *http.Request) (Task, error) {
+	// converts json body to Task struct applying sane defaults if any are missing
 	// initilise myTask with the following defaults
-	myTask := Task {
-		ID: 0,
-        Duration: 5,
-		QP: 2,
-		MsgSize: 8383608,
+	myTask := Task{
+		ID:                     0,
+		Duration:               5,
+		QP:                     2,
+		MsgSize:                8383608,
 		IgnoreCPUSpeedWarnings: true,
 	}
 	// the next part reads in json body data and applies any appropriate data to the struct
-	decoder:=json.NewDecoder(r.Body)
+	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&myTask)
 	return myTask, err
 }
@@ -64,7 +65,7 @@ func taskWorker(index int) {
 		if err != nil {
 			fmt.Printf("in taskworker %v\n", err)
 			var myResult TaskResult
-			myResult.Error=err
+			myResult.Error = err
 			task.OutputChannel <- myResult
 		}
 		fmt.Printf("Task ID %d completed\n", task.ID)
@@ -75,22 +76,22 @@ func submitTask(w http.ResponseWriter, r *http.Request) {
 
 	// Create a new task and add it to the queue
 	// set the task id to
-	task, err := parseBodyToTask( r )
-    if err != nil {
-		http.Error(w, fmt.Sprintf("{\"StatusCode\": 2, \"ErrorText\": \"Failed to Parse the Body you sent as JSON. You sent: '%s'\"}",r.Body), http.StatusBadRequest)
+	task, err := parseBodyToTask(r)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("{\"StatusCode\": 2, \"ErrorText\": \"Failed to Parse the Body you sent as JSON. You sent: '%s'\"}", r.Body), http.StatusBadRequest)
 		return
 	}
 	//var output OutputQueue
 	output := make(chan TaskResult)
-	task.ID=taskID
-	task.OutputChannel=output
+	task.ID = taskID
+	task.OutputChannel = output
 	select {
 	case taskQueue <- task:
 		taskID = taskID + 1
-		result := <- output
+		result := <-output
 		if result.Error != nil {
-			response := JsonErrorResponse{ErrorText: fmt.Sprintf("An error occured in the server side: %s",result.Error), StatusCode: 1}
-			jsonString , err := json.Marshal(response)
+			response := JsonErrorResponse{ErrorText: fmt.Sprintf("An error occured in the server side: %s", result.Error), StatusCode: 1}
+			jsonString, err := json.Marshal(response)
 			if err == nil {
 				w.Header().Set("Content-Type", "application/json")
 				w.WriteHeader(http.StatusInternalServerError)
@@ -102,20 +103,20 @@ func submitTask(w http.ResponseWriter, r *http.Request) {
 		} else {
 			response := JsonResponse{TaskData: task, MessageText: "started an ib_write_bw process sucessfully", StatusCode: 0, ServerPort: result.ServerPort}
 			w.Header().Set("Content-Type", "application/json")
-            jsonString , _ := json.Marshal(response)
+			jsonString, _ := json.Marshal(response)
 			w.Write(jsonString)
 
 		}
 	default:
 		// If channel is full or there are too many tasks running, reject new task
 		w.Header().Set("Content-Type", "application/json")
-		http.Error(w,"{\"StatusCode\": 5, \"ErrorText\": \"Try again later all NICs in use\"}", http.StatusTooManyRequests)
+		http.Error(w, "{\"StatusCode\": 5, \"ErrorText\": \"Try again later all NICs in use\"}", http.StatusTooManyRequests)
 	}
 }
 
 func main() {
 	NicList = []string{"bnxt_re0", "bnxt_re1", "bnxt_re2", "bnxt_re3"}
-	maxNics=len(NicList)
+	maxNics = len(NicList)
 	// Start workers (equal to the number of available nics)
 	for i := 0; i < maxNics; i++ {
 		wg.Add(1)
--- run_perftest.go
+++ run_perftest.go
@@ -1,21 +1,22 @@
 package main
 
 import (
-	"syscall"
-	"os/exec"
 	"fmt"
+	"net"
+	"os/exec"
 	"strconv"
 	"strings"
-	"net"
+	"syscall"
 	"time"
 )
+
 const ibWriteClientWait uint64 = 5
 const ibWriteErrorWait = 100 * time.Millisecond
 
-func buildIBWriteBWArgs(myTask Task) ([]string) {
+func buildIBWriteBWArgs(myTask Task) []string {
 	//this will convert the supplied args inti
-    var arglist []string
-	arglist = append(arglist,"--duration", strconv.FormatUint(myTask.Duration, 10))
+	var arglist []string
+	arglist = append(arglist, "--duration", strconv.FormatUint(myTask.Duration, 10))
 	arglist = append(arglist, "-q", strconv.FormatUint(myTask.QP, 10))
 	arglist = append(arglist, "-s", strconv.FormatUint(myTask.MsgSize, 10))
 	if myTask.IgnoreCPUSpeedWarnings {
@@ -34,18 +35,17 @@ func Listener() (port int, err error) {
 	return
 }
 
-
-func startIBWriteBW(myTask Task, nicIndex int) (error) {
+func startIBWriteBW(myTask Task, nicIndex int) error {
 	tcpPort, _ := Listener()
 	arglist := buildIBWriteBWArgs(myTask)
 
-    arglist = append([]string{"-p", fmt.Sprintf("%d", tcpPort), "-d", fmt.Sprintf("%s", NicList[nicIndex])}, arglist...)
+	arglist = append([]string{"-p", fmt.Sprintf("%d", tcpPort), "-d", fmt.Sprintf("%s", NicList[nicIndex])}, arglist...)
 	//arglist = append([]string{"-p", fmt.Sprintf("%d", tcpPort), "-d", "bnxt_re9"}, arglist...)
 	cmd := "/opt/perftest-with-rocm/bin/ib_write_bw"
 	fmt.Printf("running %s %s\n", cmd, strings.Join(arglist, " "))
 
 	// this will start an ib_write_bw server process
-    ib_write_bw_cmd := exec.Command(cmd,  arglist... )
+	ib_write_bw_cmd := exec.Command(cmd, arglist...)
 	ib_write_bw_cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
 
 	// thanks stack overflow! https://stackoverflow.com/questions/32921792/how-do-you-kill-a-process-and-its-children-on-a-timeout-in-go-code
@@ -54,19 +54,19 @@ func startIBWriteBW(myTask Task, nicIndex int) (error) {
 	// after the timeout
 	type IBWBResult struct {
 		CombinedOutput []byte
-		Error error
+		Error          error
 	}
 	done := make(chan IBWBResult, 1)
-	    go func() {
+	go func() {
 		output, err := ib_write_bw_cmd.CombinedOutput()
 		res := IBWBResult{CombinedOutput: output, Error: err}
 		done <- res
 	}()
-	fmt.Printf( "port=%d\n",tcpPort )
+	fmt.Printf("port=%d\n", tcpPort)
 	var myResult TaskResult
 	select {
 	case ibwbresult := <-done:
-		if ibwbresult.Error != nil{
+		if ibwbresult.Error != nil {
 			return fmt.Errorf("%s\n%s\n", ibwbresult.Error, ibwbresult.CombinedOutput)
 		}
 		return nil
@@ -74,10 +74,10 @@ func startIBWriteBW(myTask Task, nicIndex int) (error) {
 		// ok so this IS a bit lame BUT:
 		// at this point we have waited for the ibWriteErrorWait duration (I set this to 100 mS as this it 5x the time it takes to fail on an unloaded system)
 		// at this point we assume all is good and the server is started so send back the port number
-		myResult.ServerPort=tcpPort
+		myResult.ServerPort = tcpPort
 		myTask.OutputChannel <- myResult
 		// now you'd think that was the end of it but now we have to wait tor the rest of the timeout period in case our job takes too long
-       time.Sleep((time.Duration(myTask.Duration + ibWriteClientWait) * time.Second) - ibWriteErrorWait)
+		time.Sleep((time.Duration(myTask.Duration+ibWriteClientWait) * time.Second) - ibWriteErrorWait)
 		// now we don't want to forever block the  task worker if the client fails to connect or catches fire mid run
 		// this next bit kills the process and frees up the worker for another job
 		pid := ib_write_bw_cmd.Process.Pid
@@ -87,7 +87,7 @@ func startIBWriteBW(myTask Task, nicIndex int) (error) {
 			err := syscall.Kill(-pgid, syscall.SIGKILL)
 			if err != nil {
 				fmt.Printf("kill Command finished with error: %v. the pid is/was %d\n", err, pid)
-				return  err
+				return err
 			}
 		}
 	}
